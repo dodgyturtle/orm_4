7449ce885265e94e84ab1c2bd1b1d76b2133393f
Aleksandr Semenov <semenlinux@gmail.com>
2021-04-12 16:57:42 +0300

    Add image filed to data model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="pokemon_images", null=True, blank=True)
 
     def __str__(self):
-        return f'{self.title}'
+        return f"{self.title}"
