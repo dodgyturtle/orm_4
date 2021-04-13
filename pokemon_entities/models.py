1bd6f6072387fd0448eae758e70341be7ccdad93
Aleksandr Semenov <semenlinux@gmail.com>
2021-04-13 11:04:55 +0300

    Add ForeignKey to data model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,3 +7,9 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
